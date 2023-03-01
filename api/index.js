129346a4fdd835406135d2e9ba5c092d822ac926
王乐源 <1577142422@qq.com>
2023-03-01 20:26:36 +0800

    feat: add test api

---
+++ api/index.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+    res.setHeader('content-type', 'text/plain');
+    res.send(`
+      This Serverless Function was built at ${new Date(BuiltTime)}.
+      The current time is ${new Date()}
+    `);
+  };
