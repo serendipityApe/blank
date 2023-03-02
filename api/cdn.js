f39b8cc1aa8bf89a7dd6db09fb71a36ca258be7c
王乐源 <1577142422@qq.com>
2023-03-02 22:23:22 +0800

    feat: get cdn test

--- api/cdn.js
+++ api/cdn.js
@@ -1,4 +1,22 @@
-module.exports = (req, res) => {
-    
-    res.send(req.toString());
-  };
+const axios = require("axios");
+const domain = "https://data.jsdelivr.com";
+
+const jsDelivr = axios.create({
+  baseURL: domain,
+});
+module.exports = async (req, res) => {
+  //React@18.2.0
+  const { dependecy } = req?.query;
+  if (!dependecy.test(/\w+@\d+/))
+    return res.json({
+      message: "invalid params",
+    });
+  const {
+    data: {
+      entrypoints: { file: entrypoint },
+    },
+  } = jsDelivr.get(`/v1/packages/npm/${dependecy}/entrypoints`);
+//   const data = await jsDelivr.get(`/v1/packages/npm/${dependecy}`);
+  res.send(`https://cdn.jsdelivr.net/npm/${dependecy}/${entrypoint}`);
+  res.send(dependecy);
+};
